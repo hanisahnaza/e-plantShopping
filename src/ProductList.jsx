fc72e93297abf4d7eb9ddb7b5483c8fd8ea931a0
hanisahnaza <nazahanisah@gmail.com>
2024-11-03 08:47:54 -0500

    Deployed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,8 +1,8 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import 'CartSlice.jsx';
-
+import { useDispatch, useSelector } from "react-redux";
+import { addItem } from "./CartSlice";
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
